0b198d02b178a2acb11a3a7680db5667e8961040
unknown <sanito78leo@hotmail.com>
2014-09-23 19:58:40 -0500

    inicio de programa

---
+++ programacion/programacion/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ALUMNOS
+ * Date: 23/09/2014
+ * Time: 07:51 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace programacion
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
+
